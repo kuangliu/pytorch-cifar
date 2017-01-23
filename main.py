191e62eb24fa26f9de1d5cf773b2af5af9ebe88c
kuangliu <kuang.liu@hotmail.com>
2017-01-23 19:18:49 +0800

    Support save/load checkpoint

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
+import os
+import argparse
+
 import numpy as np
 
 from model import *
@@ -16,7 +19,14 @@ from utils import progress_bar, msr_init
 from torch.autograd import Variable
 
 
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
+parser.add_argument('--resume', default=False, type=bool, help='resume from checkpoint')
+args = parser.parse_args()
+
 use_cuda = torch.cuda.is_available()
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
 print('==> Preparing data..')
@@ -36,9 +46,18 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
-print('==> Building model..')
-net = VGG('VGG19')
-msr_init(net.features)
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.t7')
+    net = checkpoint['net']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+else:
+    print('==> Building model..')
+    net = VGG('VGG19')
+    msr_init(net.features)
 
 if use_cuda:
     net.cuda()
@@ -50,7 +69,7 @@ optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=1e-4)
 
 # Training
 def train(epoch):
-    print('Epoch: %d' % epoch)
+    print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -74,6 +93,7 @@ def train(epoch):
             % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 def test(epoch):
+    global best_acc
     net.eval()
     test_loss = 0
     correct = 0
@@ -93,6 +113,21 @@ def test(epoch):
         progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
-for epoch in range(200):
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.module if use_cuda else net,
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.t7')
+        best_acc = acc
+
+
+for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
     test(epoch)
