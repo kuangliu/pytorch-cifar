ac48f106cf157d7094a177abaf696c6ee3867ce5
Anton Nikishin <nikishin.ar@phystech.edu>
2020-04-07 15:05:52 +0300

    Update resnet.py
    The basic block of ResNet is corrected in case of expansion rate different from one.

--- models/resnet.py
+++ models/resnet.py
@@ -18,8 +18,8 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, self.expansion*planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
