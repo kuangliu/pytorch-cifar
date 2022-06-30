1c774fa5de781ff30f2420df590d32bc2901bace
lilyjiayi <jiayili@stanford.edu>
2022-06-29 19:39:00 -0700

    data directory

--- main.py
+++ main.py
@@ -40,12 +40,12 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+    root='/jagupard30/scr1/jiayili', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+    root='/jagupard30/scr1/jiayili', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
