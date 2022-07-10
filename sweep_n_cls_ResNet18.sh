148b18bdcf2554b79fb4fb540f254e2b031c4e28
BBC <bryan.bo.cao@outlook.com>
2022-07-10 14:15:56 -0400

    Split works

---
+++ sweep_n_cls_ResNet18.sh
@@ -0,0 +1,9 @@
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 2
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 3
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 4
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 5
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 6
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 7
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 8
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 9
+python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 10
