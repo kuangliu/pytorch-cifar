bb0c72bce66061b30fa310a32e6530b3d3b1ed1d
BBC <bryan.bo.cao@outlook.com>
2022-07-10 22:05:09 -0400

    Add more bash scrpits for training

---
+++ sweep_n_cls_ResNet18_CUDA_1.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 2
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 3
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 4
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 5
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 6
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 7
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 8
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 9
+CUDA_VISIBLE_DEVICES=1 python3 main_n_cls.py --train --test --epochs 200 --net ResNet18 --num_class 10
